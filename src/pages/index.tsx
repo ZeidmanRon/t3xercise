5234d18e69927e56e544da8bd2b0909a78f3673b
Zeidman Ron <ronronz145@gmail.com>
2023-08-25 16:43:18 +0300

    styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import Head from "next/head";
-import Image from "next/image";
 import { api } from "~/utils/api";
 
 export default function Home() {
@@ -17,7 +16,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex h-screen justify-center">
-        <div className="h-full w-full border-x border-slate-400 md:max-w-2xl">
+        <div className="h-full w-full border-x border-red-400 md:max-w-2xl">
           <div className="flex border-b border-slate-400 p-4">
             {!user.isSignedIn && (
               <div className="flex justify-center">
@@ -25,13 +24,7 @@ export default function Home() {
                 <SignInButton />
               </div>
             )}
-            {user.isSignedIn && (
-              <div className="flex justify-center">
-                <CreateExerciseWizard />
-                <SignOutButton />
-                <div>hello {user.user.fullName}</div>
-              </div>
-            )}
+            {user.isSignedIn && <CreateExerciseWizard />}
           </div>
           <div className="flex flex-col">
             {[...data]?.map((exercise) => (
@@ -48,15 +41,17 @@ export default function Home() {
 
 const CreateExerciseWizard = () => {
   const { user } = useUser();
+
   if (!user) return null;
 
   return (
-    <div className="flex">
+    <div className="flex w-full gap-3">
       <img
         src={user.imageUrl}
         alt="Profile Image"
         className="h-14 w-14 rounded-full"
       />
+      <input className="bg-transparent w-full" placeholder="type here" />
     </div>
   );
 };
