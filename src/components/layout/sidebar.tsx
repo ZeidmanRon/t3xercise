e0ed02b58702ac3ed87408bd5d84f9088899d645
iZooda <ronronz145@gmail.com>
2023-09-02 18:22:15 +0300

    UI update

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import { XMarkIcon } from "@heroicons/react/24/solid";
+import React, { useState } from "react";
+import { T3href } from "./t3href";
 
 interface SidebarProps {
   isOpen: boolean;
@@ -8,18 +10,30 @@ interface SidebarProps {
 const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose }) => {
   return (
     <div
-      className={`fixed inset-0 bg-blue-800 z-40 transition-opacity ${
-        isOpen ? 'opacity-90' : 'opacity-0 pointer-events-none'
+      className={`fixed inset-0 z-40 bg-slate-50 transition-opacity ${
+        isOpen ? "opacity-100" : "pointer-events-none opacity-0"
       }`}
       onClick={onClose}
     >
       <div
-        className={`transform h-full w-3/4 bg-white fixed top-0 right-0 overflow-y-auto ease-in-out transition-transform ${
-          isOpen ? 'translate-x-0' : 'translate-x-full'
+        className={`right-0 top-0 h-full w-3/5 transform overflow-y-auto bg-white transition-transform ease-in-out ${
+          isOpen ? "translate-x-0" : "translate-x-full"
         }`}
         onClick={(e) => e.stopPropagation()}
       >
-        <button onClick={onClose}>Close</button>
+        <div className="flex w-full flex-col justify-end pt-3">
+          <div className="pr-2">
+            <button className="p-0.5" onClick={onClose}>
+              <XMarkIcon className="h-7 w-7 text-gray-700" />
+            </button>
+          </div>
+          <div className="mt-3 flex h-full flex-col justify-center">
+            <T3href text="דף הבית" href="/home" />
+            <T3href text="התרגילים שלי" href="/home" />
+            <T3href text="דף העסק" href="/home" />
+          </div>
+        </div>
+
         {/* Add your sidebar content here */}
       </div>
     </div>
