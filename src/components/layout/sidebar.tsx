39fb327d2649019b26c281d3d89cd353e51cfd98
iZooda <ronronz145@gmail.com>
2023-09-03 11:27:02 +0300

    testing h-screen in deployment

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -14,18 +14,18 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose }) => {
 
   return (
     <div
-      className={`fixed inset-0 z-40 bg-slate-50 transition-opacity ${
+      className={`fixed inset-0 z-40 h-screen bg-slate-50 transition-opacity ${
         isOpen ? "opacity-100" : "pointer-events-none opacity-0"
       }`}
       onClick={onClose}
     >
       <div
-        className={`right-0 top-0 h-screen w-3/5 transform overflow-y-auto bg-white transition-transform ease-in-out ${
+        className={`right-0 top-0 h-full w-3/5 transform overflow-y-auto bg-white transition-transform ease-in-out ${
           isOpen ? "translate-x-0" : "translate-x-full"
         }`}
         onClick={(e) => e.stopPropagation()}
       >
-        <div className="test-border flex h-screen w-full flex-col justify-normal pt-3">
+        <div className="test-border flex h-full w-full flex-col justify-normal pt-3">
           <div className="pr-2">
             <button className="p-0.5" onClick={onClose}>
               <XMarkIcon className="h-7 w-7 text-gray-700" />
@@ -36,7 +36,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose }) => {
             <T3href text="התרגילים שלי" href="/exercises" />
             <T3href text="דף העסק" href="/home" />
           </div>
-          <div className="test-border flex justify-center">
+          <div className="test-border flex h-full flex-col justify-end">
             <SignOutButton signOutCallback={() => router.push("/")}>
               <button className="rounded border border-gray-400 bg-white px-4 py-2 font-semibold text-gray-800 shadow hover:bg-gray-100">
                 Sign out
