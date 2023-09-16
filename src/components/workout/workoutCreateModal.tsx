f706202fca646ad5a95f4ab91b8d3aff50876781
Zeidman Ron <ronronz145@gmail.com>
2023-09-16 15:00:48 +0300

    patch: get amount of exercises per muscle group from select input

--- src/components/workout/workoutCreateModal.tsx
+++ src/components/workout/workoutCreateModal.tsx
@@ -13,10 +13,55 @@ import WorkoutLayout from "./workoutLayout";
 import Link from "next/link";
 
 export function WorkoutCreateModal() {
-  const [open, setOpen] = useState(false);
+  const [openWorkoutDialog, setOpenWorkoutDialog] = useState(false);
+  const [exercisesPerBooty, setExercisesPerBooty] = useState(0);
+  const [exercisesPerLegs, setExercisesPerLegs] = useState(0);
+  const [exercisesPerBack, setExercisesPerBack] = useState(0);
+  const [exercisesPerAbs, setExercisesPerAbs] = useState(0);
+  const [exercisesPerChest, setExercisesPerChest] = useState(0);
+  const [exercisesPerShoulders, setExercisesPerShoulders] = useState(0);
+  const [exercisesPerHands, setExercisesPerHands] = useState(0);
+  const [exercisesPerAerobic, setExercisesPerAerobic] = useState(0);
+
+  const values = [
+    { value: exercisesPerBooty, label: "ישבן" },
+    { value: exercisesPerLegs, label: "רגליים" },
+    { value: exercisesPerBack, label: "גב" },
+    { value: exercisesPerAbs, label: "בטן" },
+    { value: exercisesPerChest, label: "חזה" },
+    { value: exercisesPerShoulders, label: "כתפיים" },
+    { value: exercisesPerHands, label: "ידיים" },
+    { value: exercisesPerAerobic, label: "אירובי" },
+  ];
+  function handleSubmit() {
+    console.log("\n\nמספר התרגילים פר קבוצת שריר");
+    values.map((value) => {
+      console.log(value);
+    });
+    //todo 1: generate random exercises
+
+    //todo 2: create new workout with the generated exercises
+
+    //todo 3: invalidate workouts (refresh get workouts datad from the DB)
+
+    //4: reset exercises per muscleGroup values
+    setters.map(({ setter }) => {
+      setter(0);
+    });
+  }
+  const setters = [
+    { setter: setExercisesPerBooty, label: "ישבן" },
+    { setter: setExercisesPerLegs, label: "רגליים" },
+    { setter: setExercisesPerBack, label: "גב" },
+    { setter: setExercisesPerAbs, label: "בטן" },
+    { setter: setExercisesPerChest, label: "חזה" },
+    { setter: setExercisesPerShoulders, label: "כתפיים" },
+    { setter: setExercisesPerHands, label: "ידיים" },
+    { setter: setExercisesPerAerobic, label: "אירובי" },
+  ];
 
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
+    <Dialog open={openWorkoutDialog} onOpenChange={setOpenWorkoutDialog}>
       <DialogTrigger asChild>
         <Button variant={"outline"}>הוספת אימון</Button>
       </DialogTrigger>
@@ -25,7 +70,7 @@ export function WorkoutCreateModal() {
           <DialogTitle>יצירת אימון</DialogTitle>
         </DialogHeader>
         <div className="flex w-full flex-1 flex-col items-center justify-center space-y-2">
-          <WorkoutLayout />
+          <WorkoutLayout setters={setters} />
           <div className="flex items-center">
             <Switch dir="ltr" id="include-business-exercises" />
             <Label
@@ -35,7 +80,15 @@ export function WorkoutCreateModal() {
               לכלול תרגילים מיתר המאמנים בעסק?
             </Label>
           </div>
-          <Button>צור אימון</Button>
+          <form
+            onSubmit={(event) => {
+              event.preventDefault();
+              handleSubmit();
+              setOpenWorkoutDialog(false);
+            }}
+          >
+            <Button type="submit">צור אימון</Button>
+          </form>
           <Label className="pt-5">
             או{" "}
             <Link href={"#"} className="text-blue-500">
--- src/components/workout/workoutLayout.tsx
+++ src/components/workout/workoutLayout.tsx
@@ -1,28 +1,15 @@
 import * as React from "react";
 import { Label } from "~/components/ui/label";
 import { ScrollArea } from "~/components/ui/scroll-area";
-import { Separator } from "~/components/ui/separator";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "~/components/ui/select";
+import ExercisePerMuscle from "./exercisePerMuscle";
 
-const muscleGroups = [
-  { label: "ישבן", value: 0 },
-  { label: "רגליים", value: 0 },
-  { label: "גב", value: 0 },
-  { label: "בטן", value: 0 },
-  { label: "חזה", value: 0 },
-  { label: "כתפיים", value: 0 },
-  { label: "ידיים", value: 0 },
-  { label: "אירובי", value: 0 },
-];
-
-export default function WorkoutLayout() {
+interface ChildComponentProps {
+  setters: {
+    setter: React.Dispatch<React.SetStateAction<number>>;
+    label: string;
+  }[];
+}
+export default function WorkoutLayout({ setters }: ChildComponentProps) {
   return (
     <div className="flex h-full w-full flex-col items-center justify-center rounded-xl border">
       <div className="flex h-auto w-full px-2 pt-2 text-right underline">
@@ -31,28 +18,12 @@ export default function WorkoutLayout() {
 
       <div className="flex h-full w-full justify-center p-4">
         <ScrollArea dir="rtl" className=" h-auto max-h-[15rem] w-full px-4">
-          {muscleGroups.map((muscleGroup) => (
-            <div className="flex flex-col" key={muscleGroup.label}>
-              <div className="flex h-12 w-full items-center justify-between px-2 text-sm">
-                תרגילי {muscleGroup.label}
-                <Select dir="rtl">
-                  <SelectTrigger className="w-16">
-                    <SelectValue placeholder="" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectGroup>
-                      <SelectItem value="0">0</SelectItem>
-                      <SelectItem value="1">1</SelectItem>
-                      <SelectItem value="2">2</SelectItem>
-                      <SelectItem value="3">3</SelectItem>
-                      <SelectItem value="4">4</SelectItem>
-                      <SelectItem value="5">5</SelectItem>
-                    </SelectGroup>
-                  </SelectContent>
-                </Select>
-              </div>
-              <Separator className="my-2" />
-            </div>
+          {setters.map(({ setter, label }) => (
+            <ExercisePerMuscle
+              setExercisesPerMuscle={setter}
+              key={label}
+              label={label}
+            />
           ))}
         </ScrollArea>
       </div>
