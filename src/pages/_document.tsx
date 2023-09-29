dba025f30af7b32b2b7ce0f03981c60979b0dae8
Zeidman Ron <ronronz145@gmail.com>
2023-09-29 18:49:16 +0300

    disable auto zoom on text input in mobile

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,10 @@ class MyDocument extends Document {
     return (
       <Html lang="en" dir="rtl">
         <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, maximum-scale=1"
+          />
         </Head>
         <link
           href="https://fonts.googleapis.com/css2?family=Caveat:wght@400;700&display=swap"
