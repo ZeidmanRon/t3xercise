66d6d2050f0a06fdad48eb3ff33f940176cf8203
iZooda <ronronz145@gmail.com>
2023-10-03 01:39:23 +0300

    reposition website description

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,10 +4,16 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en" dir="rtl">
+        <title>T3xercise</title>
         <Head>
+          <link rel="icon" href="/favicon.ico" />
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1, maximum-scale=1"
+          />
+          <meta
+            name="description"
+            content="Unleash Your Trainer Potential: Effortlessly Craft Customized Workouts, Share Exercises, and Organize Your Fitness Arsenal – All in One Place!"
           />
         </Head>
         <link
--- src/pages/sign-in/sign-in.tsx
+++ src/pages/sign-in/sign-in.tsx
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import { useUser, SignIn } from "@clerk/nextjs";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
@@ -14,29 +13,17 @@ export default function SignInPage() {
   }, [isLoaded, isSignedIn, router, user]);
 
   return (
-    <>
-      <Head>
-        <title>T3xercise</title>
-        <meta
-          name="description"
-          content="Unleash Your Trainer Potential: Effortlessly Craft Customized Workouts, Share Exercises, and Organize Your Fitness Arsenal – All in One Place!"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className="flex flex-1 justify-center">
-        <div className="flex flex-1 flex-col items-center justify-center text-center text-stone-600">
-          <div dir="ltr">
-            <h1 className="handwrite text-[4rem] tracking-tight">T3XERCISE</h1>
-            <h1 className="handwrite -mt-5 text-[3rem] tracking-tight">
-              Build a{" "}
-              <span className="handwrite text-[hsl(247,100%,70%)]">
-                Workout
-              </span>
-            </h1>
-            <div className="mt-10">{isSignedIn ? "" : <SignIn />}</div>
-          </div>
+    <main className="flex flex-1 justify-center">
+      <div className="flex flex-1 flex-col items-center justify-center text-center text-stone-600">
+        <div dir="ltr">
+          <h1 className="handwrite text-[4rem] tracking-tight">T3XERCISE</h1>
+          <h1 className="handwrite -mt-5 text-[3rem] tracking-tight">
+            Build a{" "}
+            <span className="handwrite text-[hsl(247,100%,70%)]">Workout</span>
+          </h1>
+          <div className="mt-10">{isSignedIn ? "" : <SignIn />}</div>
         </div>
-      </main>
-    </>
+      </div>
+    </main>
   );
 }
