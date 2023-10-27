5dcfa271c243e6fac6b16a46ebd5670964e087e5
Zeidman Ron <ronronz145@gmail.com>
2023-10-27 12:50:01 +0300

    add equipment list

--- src/components/globals.tsx
+++ src/components/globals.tsx
@@ -3,9 +3,21 @@ export const muscleGroups = [
   { label: "בטן", value: "בטן" },
   { label: "גב", value: "גב" },
   { label: "חזה", value: "חזה" },
-  { label: "ידיים", value: "ידיים" },
+  { label: "יד אחורית", value: "יד אחורית" },
+  { label: "יד קדמית", value: "יד קדמית" },
   { label: "ישבן", value: "ישבן" },
   { label: "כתפיים", value: "כתפיים" },
   { label: "ליבה", value: "ליבה" },
   { label: "רגליים", value: "רגליים" },
+];
+
+export const equipment = [
+  { label: "מוט", value: "מוט" },
+  { label: "TRX", value: "TRX" },
+  { label: "פיתה", value: "פיתה" },
+  { label: "גומיה", value: "גומיה" },
+  { label: "קטלבלס", value: "קטלבלס" },
+  { label: "דמבלס", value: "דמבלס" },
+  { label: "מוט", value: "מוט" },
+  { label: "מוט", value: "מוט" },
 ];
